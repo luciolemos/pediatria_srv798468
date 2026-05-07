e8b81d10e474d87adfc404142e62f3a350707e41
luciolemos <luciolemos.j5@gmail.com>
2026-05-07 11:23:58 +0000

    pediatria

--- config/content/landing.php
+++ config/content/landing.php
@@ -18,7 +18,7 @@ return [
         ],
         'schema' => [
             'type' => 'MedicalClinic',
-            'logo' => 'assets/img/clinic-mark.svg',
+            'logo' => 'assets/img/pediatria-mark.svg',
             'price_range' => '$$',
             'area_served' => 'Natal e região',
             'include_services' => true,
